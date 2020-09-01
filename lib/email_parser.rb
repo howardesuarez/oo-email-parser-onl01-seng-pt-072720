f893ff64762a3f40ec6db76e3c3f9f06386ff89f
howard suarez <howardesuarez@gmail.com>
2020-09-01 06:01:41 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,14 +4,19 @@
 # or whitespace (' ').
 
 class EmailParser
-attr_accessor :emails
 
-def initialize(email)
-  @emails = email
+  attr_reader :addresses_raw
+
+  def initialize(addresses_string)
+    @addresses_raw = addresses_string
+  end
+
+  def parse
+    #need to review the bottom 2 methods bec I don't understand them -
+    #Blog post on regex????
+    addresses_raw.split(",").join.split.uniq
+    #addresses_raw.split(/[\s,]+/).uniq
+    #addresses_raw.split(/ |, |,/).uniq
+  end
+
 end
-
-def parse
-  emails.delete(',').split.uniq
-end
-
-end 
