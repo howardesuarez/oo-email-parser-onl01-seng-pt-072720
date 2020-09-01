b09938368c9463ef4293037408b6ae1b4507ec70
howard suarez <howardesuarez@gmail.com>
2020-09-01 06:03:06 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,20 +3,18 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 
+require 'set'
+
 class EmailParser
 
-  attr_reader :addresses_raw
 
-  def initialize(addresses_string)
-    @addresses_raw = addresses_string
+  attr_reader :emails
+
+  def initialize(emails)
+    @emails = emails
   end
 
   def parse
-    #need to review the bottom 2 methods bec I don't understand them -
-    #Blog post on regex????
-    addresses_raw.split(",").join.split.uniq
-    #addresses_raw.split(/[\s,]+/).uniq
-    #addresses_raw.split(/ |, |,/).uniq
+    @emails.split(/[,\s]+/).map{|x| x.strip}.to_set.to_a
   end
-
 end
